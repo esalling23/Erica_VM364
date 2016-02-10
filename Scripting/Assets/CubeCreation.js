accca4079de99f1f0f511ab3ecc5e95590042808
Erica Salling <esalling23@gmail.com>
2016-02-10 17:46:12 -0500

    day one

---
+++ Scripting/Assets/CubeCreation.js
@@ -0,0 +1,40 @@
+﻿#pragma strict
+
+var cubeCreate = true;
+var cube : GameObject;
+
+var projectile : Rigidbody;
+
+function Start () {
+	if (cubeCreate == true){
+		for (var i : int = -5;i < 5; i++) {
+	    	    Instantiate (cube, Vector3(i * 4.0, 0, 0), Quaternion.identity);
+		}
+	   cubeCreate = false;
+	}
+}
+
+
+function Update () {
+    // Ctrl was pressed, launch a projectile
+    if (Input.GetButtonDown("Fire1")) {
+
+         var mousePos = Input.mousePosition;
+         mousePos.z = 1.0;       
+         var objectPos = Camera.main.ScreenToWorldPoint(mousePos);
+
+        // Instantiate the projectile at the position and rotation of this transform
+        var clone : Rigidbody;
+
+        clone = Instantiate(projectile, objectPos, transform.rotation);
+        
+        // Give the cloned object an initial velocity along the current 
+        // object's Z axis
+        clone.velocity = transform.TransformDirection (Vector3.forward * 10);
+    }
+
+
+
+
+}
+
---
+++ Scripting/Assets/CubeMovement.js
@@ -0,0 +1,30 @@
+﻿#pragma strict
+
+var cubeUp = false;
+
+function Start () {
+
+}
+
+function Update () {
+		//transform.Translate(Vector3.up * Time.deltaTime, Space.World);
+
+		if (gameObject.transform.position.y >5) {
+		cubeUp = false;
+		}
+
+		if (gameObject.transform.position.y <-5) {
+		cubeUp = true;
+		}
+
+
+
+		if (cubeUp == false) {
+		transform.Translate(Vector3.down * Time.deltaTime, Space.World);
+		}
+
+		if (cubeUp == true) {
+		transform.Translate(Vector3.up * Time.deltaTime, Space.World);
+		}
+
+}
